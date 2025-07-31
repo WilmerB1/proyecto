4efe537523da63519fee465d420e838f9aee6a35
WilmerB1 <bdw9_10@hotmail.com>
2025-07-30 21:24:25 -0500

    Se agrego stopwords a main

---
+++ main.py
@@ -0,0 +1,8 @@
+# importar la libreria de nltk 
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+# definir ls ruta donde se almacenaran los datos descargados nltk
+nltk.data.path.append('C:/Users/elkin/AppData/Roaming/nltk_data')
